6c30c83c3028744bec95a4e4ffddd065137056f4
George Foot <george.foot@gmail.com>
2014-04-16 16:21:03 +0100

    Add asset postprocessor to sync .lua files into Resources/LuaScripts

---
+++ Assets/Editor/LuaScriptAssetPostprocessor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class LuaScriptAssetPostprocessor : AssetPostprocessor
+{
+	const string SourcePrefix = "Assets/LuaScripts/";
+	const string DestPrefix = "Assets/Resources/LuaScripts/";
+
+	static void OnPostprocessAllAssets(
+		string[] importedAssets,
+		string[] deletedAssets,
+		string[] movedAssets,
+		string[] movedFromAssetPaths
+	)
+	{
+		foreach (var path in importedAssets)
+		{
+			if (path.StartsWith(SourcePrefix) && path.EndsWith(".lua"))
+			{
+				var newPath = DestPrefix + path.Substring(SourcePrefix.Length) + ".txt";
+				Debug.Log(string.Format("{0} => {1}", path, newPath));
+				var result = AssetDatabase.CopyAsset(path, newPath);
+				Debug.Log(result);
+				AssetDatabase.ImportAsset(newPath);
+			}
+		}
+	}
+}
